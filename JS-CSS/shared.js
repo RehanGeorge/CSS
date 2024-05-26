7ffa742051b469f8d73d5c3c54d0b831f6c4af8c
RehanGeorge <rehan.george@gmail.com>
2024-05-26 20:39:19 +0530

    Add modal and backdrop functionality when plan button is clicked

---
+++ JS-CSS/shared.js
@@ -0,0 +1,15 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButton = document.querySelectorAll(".plan button");
+
+const modalNo = document.querySelector(".modal button");
+
+// console.log(backdrop);
+
+// Show modal and backdrop
+selectPlanButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+});
